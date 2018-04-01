498cf0b8496a213ec97e190fcbe440d5c4a37dd8
fanhl <dyguests@qq.com>
2018-04-01 12:37:36 +0800

    drag

--- app/src/main/java/com/fanhl/fashion/ui/main/MainActivity.kt
+++ app/src/main/java/com/fanhl/fashion/ui/main/MainActivity.kt
@@ -3,10 +3,15 @@ package com.fanhl.fashion.ui.main
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.support.constraint.ConstraintSet
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.transition.TransitionManager
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
+import android.view.ViewGroup
 import com.fanhl.fashion.R
 
 import kotlinx.android.synthetic.main.activity_main.*
@@ -15,7 +20,22 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val root = LayoutInflater.from(this).inflate(R.layout.activity_main, null as ViewGroup?) as ConstraintLayout
+        setContentView(root)
+
+        ll_top.setOnClickListener {
+            TransitionManager.beginDelayedTransition(root)
+            guideline.layoutParams = (guideline.layoutParams as ConstraintLayout.LayoutParams).apply { guidePercent = 1f }
+        }
+
+        v_logo.setOnClickListener {
+            TransitionManager.beginDelayedTransition(root)
+            guideline.layoutParams = (guideline.layoutParams as ConstraintLayout.LayoutParams).apply { guidePercent = .5f }
+        }
+        ll_bottom.setOnClickListener {
+            TransitionManager.beginDelayedTransition(root)
+            guideline.layoutParams = (guideline.layoutParams as ConstraintLayout.LayoutParams).apply { guidePercent = 0f }
+        }
     }
 
     override fun onBackPressed() {
